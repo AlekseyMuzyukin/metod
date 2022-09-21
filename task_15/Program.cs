4242884bf0fc4a4484edfb6383bdb62c03bfc8c2
Aleksey <J2k_pot@mail.ru>
2022-09-21 06:23:02 +0300

    13_00

--- task_15/Program.cs
+++ task_15/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите номер дня недели: ");
+string? numDay = Console.ReadLine();
+if (numDay == "1")
+{
+    Console.WriteLine ("Рабочий день");
+}
+else if (numDay == "2")
+{
+    Console.WriteLine ("Рабочий день");
+} 
+else if (numDay == "3")
+{
+    Console.WriteLine ("Рабочий день");
+} 
+else if (numDay == "4")
+{
+    Console.WriteLine ("Рабочий день");
+} 
+else if (numDay == "5")
+{
+    Console.WriteLine ("Рабочий день");
+} 
+else if (numDay == "6")
+{
+    Console.WriteLine ("Это выходной Суббота");
+} 
+else if (numDay == "7")
+{
+    Console.WriteLine ("Это выходной Воскресение");
+} 
+else
+{
+    Console.WriteLine ("Вы ввели неверное значение");
+} 
